a3890cbc72e34066d01ba9fe52339948bae31501
chakrigatti <chakradhar.g@zohocorp.com>
2025-05-27 22:35:54 +0530

    Add tutorial on centering a div and log font data functionality

---
+++ posts/test-posts/script.js
@@ -0,0 +1,17 @@
+async function logFontData() {
+    try {
+      const availableFonts = await window.queryLocalFonts();
+      for (const fontData of availableFonts) {
+        console.log(fontData.postscriptName);
+        console.log(fontData.fullName);
+        console.log(fontData.family);
+        console.log(fontData.style);
+      }
+    } catch (err) {
+      console.error(err.name, err.message);
+    }
+}
+
+document.getElementById('fetchFontsButton').onclick = () => {
+    logFontData();
+}
